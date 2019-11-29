ed387b2e681085afb1dc0f748f0fcbcd5a41b378
hyson666666 <2854529108@qq.cpm>
2019-11-29 21:50:56 +0800

    首次

---
+++ src/main/java/com/tx/tx_11_29/Tx1129Application.java
@@ -0,0 +1,13 @@
+package com.tx.tx_11_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tx1129Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tx1129Application.class, args);
+    }
+
+}
