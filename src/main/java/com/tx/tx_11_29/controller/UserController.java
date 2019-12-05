f98fc3b78ce4f79beb8860af69810df705e8cc9f
hyson666666 <2854529108@qq.cpm>
2019-12-05 20:42:00 +0800

    layui

--- src/main/java/com/tx/tx_11_29/controller/UserController.java
+++ src/main/java/com/tx/tx_11_29/controller/UserController.java
@@ -4,7 +4,6 @@ import com.tx.tx_11_29.entity.TbUser;
 import com.tx.tx_11_29.service.ITbUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
